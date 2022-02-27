b58e9c68a227c506ff71b216c6cea4306ecf814e
carotkut94 <carotkut12@gmail.com>
2022-02-28 01:03:36 +0530

    feat(): wraps up all components

--- app/src/main/java/com/death/gallery/components/GalleryPreview.kt
+++ app/src/main/java/com/death/gallery/components/GalleryPreview.kt
@@ -5,13 +5,14 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 
 @Composable
 fun GalleryPreview(modifier: Modifier = Modifier, selectedImage: Uri) {
-    Box(modifier = modifier.background(Color.Black)) {
+    Box(modifier = modifier.background(Color.Black), contentAlignment = Alignment.Center) {
         GalleryImage(
             uri = selectedImage,
             scaleType = ContentScale.None,
